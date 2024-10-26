6b2290a40872660cee700ba0db8bd49c9f321764
choijian <an032944@daum.net>
2024-10-26 16:53:51 +0900

    Feat: camera 연동

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.routers import video
 
 app = FastAPI()
 
@@ -15,3 +16,5 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
+
+app.include_router(video.router)
