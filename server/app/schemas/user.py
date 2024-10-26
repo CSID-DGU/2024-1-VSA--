1c9f1eebff4e2d3346602a50324d95b3a54c1e81
choijian <an032944@daum.net>
2024-10-26 16:53:51 +0900

    Feat: camera 연동

--- server/app/schemas/user.py
+++ server/app/schemas/user.py
@@ -1,13 +1,1 @@
 from pydantic import BaseModel
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
-
-class UserOut(BaseModel):
-    id: int
-    name: str
-    email: str
-
-    class Config:
-        orm_mode = True  # SQLAlchemy 모델과 호환되도록 설정
